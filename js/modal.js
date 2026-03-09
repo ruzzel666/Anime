71d63cd2bdb432b96545c92cd4e19ed4bd3f9c14
Georgy Zaitsev <90213591+ruzzel666@users.noreply.github.com>
2026-03-10 00:32:30 +0500

    Добавление модального окна
    Написал функции открывания и закрывания модального окна и Прелоадера. А также повесил обработчик событий на поле вводе в модальном окне. Результат из поля ввода выводиться в консоль

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+const modal = document.querySelector('.search-model')
+const modalBtn = document.querySelector('.icon_search')
+const modalClose = modal.querySelector('.search-close-switch')
+const searchInput = modal.querySelector('#search-input')
+
+modalBtn.addEventListener('click', () => {
+    modal.style.display = 'block'
+})
+
+modalClose.addEventListener('click', () => {
+    modal.style.display = 'none'
+})
+
+searchInput.addEventListener('change', (e) => {
+    console.log(e.target.value)
+})
