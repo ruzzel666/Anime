bd81f1302c18bc2ba340cab1b45184ee33ccda21
Georgy Zaitsev <90213591+ruzzel666@users.noreply.github.com>
2026-03-10 14:48:13 +0500

    Добавил плавный скролл страницы и слайдер сайдбара в начале страницы

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,20 @@
-const modal = document.querySelector('.search-model')
-const modalBtn = document.querySelector('.icon_search')
-const modalClose = modal.querySelector('.search-close-switch')
-const searchInput = modal.querySelector('#search-input')
+const modal = () => {
+    const modal = document.querySelector('.search-model')
+    const modalBtn = document.querySelector('.icon_search')
+    const modalClose = modal.querySelector('.search-close-switch')
+    const searchInput = modal.querySelector('#search-input')
 
-modalBtn.addEventListener('click', () => {
-    modal.style.display = 'block'
-})
+    modalBtn.addEventListener('click', () => {
+        modal.style.display = 'block'
+    })
 
-modalClose.addEventListener('click', () => {
-    modal.style.display = 'none'
-})
+    modalClose.addEventListener('click', () => {
+        modal.style.display = 'none'
+    })
 
-searchInput.addEventListener('change', (e) => {
-    console.log(e.target.value)
-})
+    searchInput.addEventListener('change', (e) => {
+        console.log(e.target.value)
+    })
+}
+
+modal()
